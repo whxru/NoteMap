c0f002f2662ab3e6446afeb100658eff52876e31
whxru <seuhuangziyao@outlook.com>
2018-02-09 22:14:47 +0800

    Smaller scope of the variable

--- src/utils/access-account.js
+++ src/utils/access-account.js
@@ -1,8 +1,8 @@
-const Evernote = require('evernote');
 const { EvernoteAccount } = require('../evernote/evernote-account')
 
 module.exports = {
     accessAccount: () => {
+        const Evernote = require('evernote');
         const developerToken = require('../../config.json').developerToken;
         var client = new Evernote.Client({ token: developerToken });
         return new EvernoteAccount(client.getUserStore(), client.getNoteStore());
