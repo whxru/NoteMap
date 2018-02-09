24905a44a651cb096fd69f8c8ff5259ba852a792
whxru <seuhuangziyao@outlook.com>
2018-02-09 22:13:15 +0800

    Mark the comment out in example code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { accessAccount } = require('./utils/access-account');
+const { buildWindow } = require('./utils/window-builder');
 
 let account = accessAccount();
-
+buildWindow();
 /** 
- * Example of using graph
+ * // Example of using graph
  * const { Graph } = require('./graph/graph')
  * var graph = new Graph('graph-container');
  * graph.addNode('node1').addNode('node2').addEdge('node1', 'node2').refresh();
