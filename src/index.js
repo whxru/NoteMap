2bf8610f0a22cc339ebcfb9ccf74c16fe02d593b
whxru <seuhuangziyao@outlook.com>
2018-02-10 15:44:22 +0800

    Initialize window and menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const { accessAccount } = require('./utils/access-account');
-const { buildWindow } = require('./utils/window-builder');
+const { buildWindow } = require('./utils/build-window');
+const { buildMenu } = require('./utils/build-menu');
 
-let account = accessAccount();
 buildWindow();
+let account = accessAccount();
+buildMenu(account);
 /** 
  * // Example of using graph
  * const { Graph } = require('./graph/graph')
- * var graph = new Graph('graph-container');
+ * var graph = new Graph('#graph-container');
  * graph.addNode('node1').addNode('node2').addEdge('node1', 'node2').refresh();
  */
