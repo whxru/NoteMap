4ea1a0dcdd9a236fb4c871a82ff807cf58c84171
whxru <seuhuangziyao@outlook.com>
2018-02-16 12:11:50 +0800

    Add CodeMirror as markdown editor

--- src/builds/build-editor.js
+++ src/builds/build-editor.js
@@ -2,9 +2,19 @@ const { ipcRenderer } = require('electron')
 
 module.exports = {
     buildEditor: () => {
-        ipcRenderer.once('init-note', (evt, info) => {
-            console.log(info);
+        // Listen messages from main.js
+        ipcRenderer.once('init-editor', (evt, info) => {
             document.querySelector('#title').innerHTML = `- ${info.title} -`
         })
+        ipcRenderer.on('note-tree', (evt, noteTree) => {
+            // to-do: store the noteTree.
+        })
+
+        // Initialize the editor
+        var editor = CodeMirror.fromTextArea(document.querySelector('#editor'), {
+            lineNumbers: false,
+            mode: "markdown"
+        });
+        editor.setSize('100vw', 'calc(100vh - 47px)');
     }
 }
