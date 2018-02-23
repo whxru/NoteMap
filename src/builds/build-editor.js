dded7248a3cace277f4f3f752cf5b19a20ca112e
whxru <seuhuangziyao@outlook.com>
2018-02-23 13:55:58 +0800

    Preparations for select note when '@[]' has been input

--- src/builds/build-editor.js
+++ src/builds/build-editor.js
@@ -1,8 +1,26 @@
 const { ipcRenderer } = require('electron')
 const { buildMenu } = require('./build-menu');
 
+function preChar(cm, curPos) {
+    console.log(curPos);
+    if(curPos === null) return null;
+
+    var prePos = { line: curPos.line, ch: curPos.ch - 1};
+    var line = cm.getLine(prePos.line);
+    if(curPos.ch === 0) {
+        if(prePos.line === 0) return null;
+        line = cm.getLine(--prePos.line);
+        prePos.ch = line.length - 1;
+    }
+    prePos.text = line[prePos.ch];
+    console.log(prePos.text);
+    return prePos;
+}
+
 module.exports = {
     buildEditor: () => {
+        let noteTree = null;
+
         // Initialize the editor
         var editor = CodeMirror.fromTextArea(document.querySelector('#editor'), {
             lineNumbers: false,
@@ -18,8 +36,17 @@ module.exports = {
                 noteInfo: info
             });
         })
-        ipcRenderer.on('note-tree', (evt, noteTree) => {
-            // to-do: store the noteTree.
+        ipcRenderer.once('note-tree', (evt, nT) => {
+            noteTree = nT;
+        })
+
+        editor.on('change', (cm, change) => {
+            var curPos = change.from,
+                prePos;
+            if(change.text[0].endsWith(']') && (prePos=preChar(cm, curPos)).text==='[' && preChar(cm, prePos).text==='@') {
+                // cm.addWidget()
+                console.log('Prepare to select note');
+            }
         })
 
     }
